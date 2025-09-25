4ab18953538d00717c1fb76292920506b7752f30
kushal022 <kushla.learn22@gmail.com>
2025-09-25 18:23:54 +0530

    initial load cart fix

--- flipClone/client/src/App.jsx
+++ flipClone/client/src/App.jsx
@@ -1,18 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Layout from "./layouts/Layout";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { fetchCart } from "./redux/asyncThunk/cart";
 
 function App() {
-    // const { pathname } = useLocation();
+    const { pathname } = useLocation();
+    const auth = useSelector(s => s.auth)
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        dispatch(fetchCart())
+    },[])
 
-    // useEffect(() => {
-    //     window.scrollTo({
-    //         top: 0,
-    //         left: 0,
-    //         behavior: "smooth",
-    //     });
-    // }, [pathname]);
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+        });
+    }, [pathname]);
     return (
         <>
             <Layout />
--- flipClone/client/src/main.jsx
+++ flipClone/client/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from "react";
+import react,{ StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css"; // make sure to import this!
+import "react-toastify/dist/ReactToastify.css"; 
 import store from "./store/store.js";
 import { Provider } from "react-redux";
 import "slick-carousel/slick/slick.css";
